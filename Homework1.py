566790b58459b02eb9c5072a78129ab82e8c6e92
Ziolot <73664979+Ziolot@users.noreply.github.com>
2020-10-31 12:30:39 +0100

    Add files via upload

---
+++ Homework1.py
@@ -0,0 +1,40 @@
+#Task A. import this
+import this
+# You will receive The Zen of Python.
+
+# Task B
+import antigravity
+# How are you flying ? I just typed import antigravity.
+# this command will open the web site https://xkcd.com/353/
+
+#Task C
+import __hello__
+# This line will return with "Hello world1"
+
+# Task D
+from __future__ import __braces__
+# Syntax error: not a chance. *Difficult joke*
+
+#Task 4
+#tprint("Artur", font="caligraphy")
+#Hope this one is correct. It worked in terminal but I had troubles to run it in Pycharm.
+#Want to ask some question about it during our next lesson.
+
+#Task 5
+print(' \\a Bell (alert) \n \
+\\b Backspace \n \
+\\n New line \n \
+\\t Horizontal tab \n \
+\\\\ Backlash \\ \n \
+\\" Double quotation mark" \n \
+\\\' Single quotation mark\'')
+
+#Task 6
+number1=int(input('Type in Number one '))
+number2=int(input('Type in Number two '))
+numbersSum=(str(number1) + ' and ' + str(number2))
+print('You typed in ' + numbersSum)
+print(number1 + number2)
+print(number1 // number2)
+print(number1 % number2)
+print(number1 ** number2 )
