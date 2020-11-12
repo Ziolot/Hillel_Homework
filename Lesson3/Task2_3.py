49821594b8aa395e4f6bf41c2a2c9e7513464d09
Ziolot <73664979+Ziolot@users.noreply.github.com>
2020-11-12 16:04:54 +0200

    Update Task2_3.py

--- Lesson3/Task2_3.py
+++ Lesson3/Task2_3.py
@@ -1,4 +1,11 @@
 number = float(input('Type in number:'))
-Deci= number % 1
-print(round(Deci,3))
+deci= number % 1
+print(round(deci,3))
+print(int(number*10) % 10)
+ #
+  
+  
+  # OR
+number = float(input('Type in number:'))
+print(int(str(number).split('.')[1]))
 print(int(number*10) % 10)
