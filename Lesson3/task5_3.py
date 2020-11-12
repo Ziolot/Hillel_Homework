e0d0ea5b9178a6b2c628f48e19ff6f7c06a74a1c
Ziolot <73664979+Ziolot@users.noreply.github.com>
2020-11-12 15:57:59 +0200

    Update task5_3.py

--- Lesson3/task5_3.py
+++ Lesson3/task5_3.py
@@ -1,14 +1,14 @@
 import random
-number=random.randint(1,10)
+
+number = random.randint(1, 10)
 attempt = 1
-while attempt<4 :
+while attempt < 4:
     guess = int(input('Type in number from 1 to 10:'))
-    attempt=attempt+1
-    if guess != number and attempt!=4:
+    attempt = attempt + 1
+    if guess != number and attempt != 4:
         print('Try again')
-    if guess==number:
-     break
-if guess==number:
-    print('You Won!')
+    if guess == number:
+        print('You Won!')
+        break
 else:
     print('You lose')
